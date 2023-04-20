002feab2ed8d4ddce417c1951af719b8a87c1602
ishavenkatapathy <122380012+ishavenkatapathy@users.noreply.github.com>
2023-04-19 22:31:53 -0400

    delete and put in buyers + general edits to sellers

--- flask-app/src/buyers/buyers.py
+++ flask-app/src/buyers/buyers.py
@@ -68,7 +68,7 @@ def add_new_buyer():
     query += "(SELECT last_name FROM User WHERE last_name ='" + buyer_last_name + "'));" 
 
 
-    # current_app.logger.info(query)
+    current_app.logger.info(query)
 
     cursor = db.get_db().cursor()
     cursor.execute(query)
@@ -76,14 +76,46 @@ def add_new_buyer():
 
     return "Success"
 
-@buyers.route('/buyers/put', methods=['PUT'])
-def guide_update(id):
-    guide = Guide.query.get(id)
-    title = request.json['title']
-    content = request.json['content']
+# Updates a buyer
+@buyers.route('/buyers', methods=['PUT'])
+def update_buyer():
+    cursor = db.get_db().cursor()
+    req_data = request.get_json()
+    current_app.logger.info(req_data)
 
-    guide.title = title
-    guide.content = content
+    buyer_id = req_data['buyer_id']
+    phone_number = req_data['phone_number']
+    buyer_email = req_data['buyer_email']
+    street_address = req_data['street_address']
+    city = req_data['city']
+    state = req_data['state']
+    zip_code = req_data['zip_code']
+    buyer_first_name = req_data['buyer_first_name']
+    buyer_last_name = req_data['buyer_last_name']
+    
+    query = 'UPDATE INTO Buyer (buyer_id, phone_number, buyer_email, street_address, city, \
+        state, zip_code, total_seller_rating, seller_first_name, seller_last_name) \
+        VALUES ("' + str(buyer_id) + '", "' + phone_number + '", "' + buyer_email + '", "' + street_address + '", \
+            "' + city + '", "' + state + '", "' + zip_code + '", "' + buyer_first_name + '",\
+                 "' + buyer_last_name + '")'
+    
+    current_app.logger.info(query)
 
-    db.session.commit()
-    return guide_schema.jsonify(guide)
+    cursor.execute(query)
+    db.get_db().commit()
+    return "Success"
+
+# Delete the buyer with particular userID
+@buyers.route('/buyers/<buyer_id>', methods=['DELETE'])
+def delete_seller(buyer_id):
+    cursor = db.get_db().cursor()
+    cursor.execute('DELETE * from Seller where seller_id = seller_id'.format(buyer_id))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
